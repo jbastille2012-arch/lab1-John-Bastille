d56107dfb3c4bdd1dc955240d1ba4d215a2de7e1
Jack Bastille <jbastille2012@gmail.com>
2026-02-13 23:15:46 +0200

    everything is fixed in the orgainization of the folders I dont know how to use git ignore yet

---
+++ Lab1TaskSelector/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+
+        bool allowChallenge = args.Contains("--challenge");
+        int max = allowChallenge ? 5 : 4;
+        int task = rnd.Next(1, max + 1);
+
+        if (task == 5)
+        {
+            int fallback = rnd.Next(1, 5);
+            Console.WriteLine($"Challenge task selected: 5");
+            Console.WriteLine($"Fallback task: {fallback}");
+        }
+        else
+        {
+            Console.WriteLine($"Your task is: {task}");
+        }
+    }
+}
