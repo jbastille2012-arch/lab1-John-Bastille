a84a24062bbd3d386418a0a3b2399776bf9a1087
Jack Bastille <jbastille2012@gmail.com>
2026-02-25 23:27:04 +0200

    this should merge all the files correctly

--- lab1/JohnBastille/Task1/lab1/JohnBastille/Task1/Student.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class Student 
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public double AverageGrade { get; set; }
-}
